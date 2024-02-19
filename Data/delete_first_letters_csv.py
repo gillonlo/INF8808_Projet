fb55c3bb264349ee4077a6f964fe26306d080f5a
gillonlo <74982380+gillonlo@users.noreply.github.com>
2024-02-19 17:55:46 -0500

    [ADD] opponents

--- Data/delete_first_letters_csv.py
+++ Data/delete_first_letters_csv.py
@@ -6,7 +6,7 @@ def delete_first_three_letters(filename):
     print(df)
 
     # Remove the first three letters from the "Squad" column
-    df['Team1'] = df['Team1'].apply(lambda x: x[:-3] if isinstance(x, str) and len(x) > 3 else x)
+    df['Squad'] = df['Squad'].apply(lambda x: x[3:] if isinstance(x, str) and len(x) > 3 else x)
 
     # Write the modified DataFrame to a new CSV file
     new_filename = filename.replace('.csv', '_modified.csv')
@@ -14,5 +14,5 @@ def delete_first_three_letters(filename):
     print(f"Modified data has been saved to '{new_filename}'.")
 
 # Example usage:
-filename = 'projet_data_3_modified.csv'  # Replace 'data.csv' with the path to your CSV file
+filename = 'projet_data_4.csv'  # Replace 'data.csv' with the path to your CSV file
 delete_first_three_letters(filename)
