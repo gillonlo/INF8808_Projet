ba88f51f03c8db7a620f0fa8af3bb88c8b2cf75e
Marouane <marouanne.oud@outlook.fr>
2024-03-28 04:42:53 -0400

    add africa map

--- Code/visu4.py
+++ Code/visu4.py
@@ -2,26 +2,70 @@ import dash
 import pandas as pd
 from dash import dcc, html
 import plotly.graph_objs as go
+import plotly.express as px
 from dash.dependencies import Input, Output
+from app_init import app
 
-def get_section(data : pd.DataFrame, team : str) -> dcc.Graph :
-    return [dcc.Graph(
-            id='visu_4',
-            figure=get_figure(data=data, team=team),
-            style={'width': '70%', 'margin': '0 auto'})
-            ]
+def get_section(data: pd.DataFrame, team: str) -> list:
+    return [
+        html.Div([
+            dcc.RadioItems(
+                id='criteria',
+                options=[
+                    {'label': '  Attaque', 'value': 'Attaque'},
+                    {'label': '  Défense', 'value': 'Défense'},
+                ],
+                value='Attaque',
+                labelStyle={'display': 'inline-block', 'margin-right': '10px'},
+                style={'border': '1px solid #ccc', 'padding': '10px', 'display': 'inline-block', 'margin-left': '20%'}
+            ),
+            dcc.Graph(
+                id='visu_4',
+                figure=get_figure(data=data, team=team),
+                style={'width': '70%', 'margin': '0 auto'}
+            ),
+            html.Div(id='output-container-radio'),  # Ajouter un élément de sortie pour l'option sélectionnée
+            html.Div(id='output-container-map')  # Ajouter un élément de sortie pour la région sélectionnée sur la carte
+        ])
+    ]
 
-def get_figure(data : pd.DataFrame, team : str) -> dict :
-    # EXEMPLE : linechart avec toy dataset
-    fig = {
-        'data': [
-            go.Scatter(
-                x=[1, 2, 3, 4, 5],
-                y=[10, 15, 13, 17, 20]
-            )
-        ],
-        'layout': go.Layout(
-            title='Line Chart'
-        )
-    }
+def get_figure(data: pd.DataFrame, team: str) -> dict:
+    # Créer une carte de l'Afrique avec des régions colorées
+    fig = px.choropleth(data, 
+                        locations="Squad",  # Colonnes des noms de pays
+                        locationmode="country names",  # Mode de localisation des pays
+                        color="Region",  # Colonne de la région
+                        hover_name="Region",  # Nom à afficher au survol
+                        color_discrete_map={"Nord": "blue", "Australe": "orange", "Ouest": "red", "Centrale": "green", "Est": "yellow"},
+                        scope="africa")
+
+    fig.update_geos(showcountries=True, showcoastlines=False, showland=False, fitbounds="locations", showframe=False)
+    fig.update_traces(hovertemplate="<b>%{hovertext}</b><br>")
+
+    # Désactiver le zoom et le panoramique
+    fig.update_layout(
+        dragmode=False,
+        uirevision=True,
+        margin=dict(l=0, r=50, t=0, b=0),  # Réduire les marges
+        clickmode='event+select',  # Activer le mode de clic pour récupérer la région sélectionnée
+    )
+
     return fig
+
+@app.callback(
+    Output('output-container-radio', 'children'),
+    [Input('criteria', 'value')]
+)
+def display_selected_option(value):
+    print( f"The selected option is: {value}")
+
+@app.callback(
+    Output('output-container-map', 'children'),
+    [Input('visu_4', 'selectedData')]
+)
+def display_selected_region(selectedData):
+    if selectedData:
+        selected_region = selectedData['points'][0]['hovertext']
+        print( f"You selected the {selected_region} region.")
+
+
--- Code/visu4_pretraitement.py
+++ Code/visu4_pretraitement.py
@@ -5,4 +5,34 @@ def get_data(data_teams_attack : pd.DataFrame,
              data_players : pd.DataFrame, 
              data_tournament : pd.DataFrame, 
              data_teams_defense : pd.DataFrame) -> pd.DataFrame :
-    return pd.DataFrame()
+    
+    regions = {
+    'Algeria': 'Nord',
+    'Angola': 'Australe',
+    'Burkina Faso': 'Ouest',
+    'Cameroon': 'Centre',
+    'Cape Verde': 'Ouest',
+    'Congo DR': 'Centre',
+    "Côte d'Ivoire": 'Ouest',
+    'Egypt': 'Nord',
+    'Equ. Guinea': 'Centre',
+    'Gambia': 'Ouest',
+    'Ghana': 'Ouest',
+    'Guinea': 'Ouest',
+    'Guinea-Bissau': 'Ouest',
+    'Mali': 'Ouest',
+    'Mauritania': 'Nord',
+    'Morocco': 'Nord',
+    'Mozambique': 'Australe',
+    'Namibia': 'Australe',
+    'Nigeria': 'Ouest',
+    'Senegal': 'Ouest',
+    'South Africa': 'Australe',
+    'Tanzania': 'Est',
+    'Tunisia': 'Nord',
+    'Zambia': 'Australe'
+}
+
+    # Ajouter une colonne de région en utilisant le dictionnaire de mapping
+    data_teams_attack['Region'] = data_teams_attack['Squad'].map(regions)
+    return data_teams_attack
