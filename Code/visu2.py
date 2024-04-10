53161ac840c695a48fe005026111c8171093515f
CarrieKam <satoshiminamisama@gmail.com>
2024-04-09 22:15:30 -0400

    [DEV] Attaque Defense radio button fixed

--- Code/visu2.py
+++ Code/visu2.py
@@ -7,84 +7,95 @@ from dash import dcc, html
 import plotly.graph_objs as go
 from dash.dependencies import Input, Output, State
 
-def get_section(data : pd.DataFrame, team : str) -> list :
+
+def get_section(data: pd.DataFrame, team: str) -> list:
     global DATA
     DATA = copy.deepcopy(data)
     global TEAM
     TEAM = copy.deepcopy(team)
     return [html.Div([
-                dcc.RadioItems(
-                    id='criteria',
-                    options=[
-                        {'label': '  Attaque', 'value': 'Attaque'},
-                        {'label': '  Défense', 'value': 'Défense'},
-                    ],
-                    value='Attaque',
-                    labelStyle={'display': 'inline-block', 'margin-right': '10px'}
-                ),
-            ], style={'border': '1px solid #ccc', 'padding': '10px', 'display': 'inline-block', 'margin-left': '20%'}),
-            
-            html.Div(id='polar-charts')
-            ]
+        dcc.RadioItems(
+            id='criteriaPolar',
+            options=[
+                {'label': '  Attaque', 'value': 'Attaque'},
+                {'label': '  Défense', 'value': 'Défense'},
+            ],
+            value='Attaque',
+            labelStyle={'display': 'inline-block',
+                        'margin-right': '10px'}
+        ),
+    ], style={'border': '1px solid #ccc', 'padding': '10px', 'display': 'inline-block', 'margin-left': '20%'}),
+
+        html.Div(id='polar-charts')
+    ]
+
 
 @app.callback(
     Output('polar-charts', 'children'),
-    [Input('criteria', 'value')]
+    [Input('criteriaPolar', 'value')]
 )
-def update_output(selected_option : str) -> list :
-    if selected_option == 'Attaque' : 
-        new_data = DATA.sort_values(by=['Attack_Sum'], ascending=False).reset_index(drop=True)
-        index = [i for i in range(len(new_data)) if new_data.iloc[i]['Squad'] == TEAM]
-        if index[0] <= 10 :
+def update_output(selected_option: str) -> list:
+    if selected_option == 'Attaque':
+        new_data = DATA.sort_values(
+            by=['Attack_Sum'], ascending=False).reset_index(drop=True)
+        index = [i for i in range(len(new_data))
+                 if new_data.iloc[i]['Squad'] == TEAM]
+        if index[0] <= 10:
             index = [9]
-        categories = ['Attack_Poss', 'Attack_Goals', 'Attack_Assists', 'Attack_Non_Penalty_Goals']
-        return [
-            html.Div(
-                [dcc.Graph(
-                    id='visu_2_'+str(i),
-                    figure=get_figure(data=new_data.iloc[i], team=TEAM, categories=categories, classement=i),
-                    style={'width': '20%', 'margin': '0 auto', 'display': 'inline-block'})
-                    for i in list(range(9))+index
-                ],
-                className='row')
-            ]
-    else : 
-        new_data = DATA.sort_values(by=['Defense_Sum'], ascending=False).reset_index(drop=True)
-        index = [i for i in range(len(new_data)) if new_data.iloc[i]['Squad'] == TEAM]
-        if index[0] <= 10 :
-            index = [9]
-        categories = ['Defense_Poss', 'Defense_Goals', 'Defense_Assists', 'Defense_Non_Penalty_Goals']
-        return [
-            html.Div(
-                [dcc.Graph(
-                    id='visu_2_'+str(i),
-                    figure=get_figure(data=new_data.iloc[i], team=TEAM, categories=categories, classement=i),
-                    style={'width': '20%', 'margin': '0 auto', 'display': 'inline-block'})
-                    for i in list(range(9))+index
-                ],
-                className='row')
-            ]
+        categories = ['Passes tentées', 'Buts marqués',
+                      'Passes décisives', 'Buts sans pénalité']
 
-def get_figure(data : pd.DataFrame, team : str, categories : list[str], classement : int) -> dict :
+    else:
+        new_data = DATA.sort_values(
+            by=['Defense_Sum'], ascending=False).reset_index(drop=True)
+        index = [i for i in range(len(new_data))
+                 if new_data.iloc[i]['Squad'] == TEAM]
+        if index[0] <= 10:
+            index = [9]
+        categories = ['Passes adverses tentées', 'Buts reçus',
+                      'Passes décisives adverses', 'Buts sans pénalités reçus']
+    return [
+        html.Div(
+            [dcc.Graph(
+                id='visu_2_'+str(i),
+                figure=get_figure(
+                    data=new_data.iloc[i], team=TEAM, criteria=selected_option, categories=categories, classement=i),
+                style={'width': '20%', 'margin': '0 auto', 'display': 'inline-block'})
+                for i in list(range(9))+index
+             ],
+            className='row')
+    ]
+
+
+def get_figure(data: pd.DataFrame, team: str, criteria: str, categories: list[str], classement: int) -> dict:
     squad_name = data['Squad']
-    values = data[categories].tolist()
-    
-    if squad_name == team :
+
+    if criteria == 'Attaque':
+        values = data[['Passes tentées', 'Buts marqués',
+                       'Passes décisives', 'Buts sans pénalité']].tolist()
+    else:
+        values = data[['Passes adverses tentées', 'Buts reçus',
+                       'Passes décisives adverses', 'Buts sans pénalités reçus']].tolist()
+
+    if squad_name == team:
         color = 'rgba(255, 0, 0, 0.3)'
-    else :
+    else:
         color = 'rgba(169, 169, 169, 0.1)'
-    
+
     fig = go.Figure()
 
     # Add trace
     fig.add_trace(go.Barpolar(
         r=values,
         theta=categories,
-        name=squad_name
+        name=squad_name,
+        hoverinfo="text",
+        hovertemplate="%{theta}<br>""%{r}<extra></extra>",
     ))
-    
+
     fig.update_layout(
-        title=str(classement+1)+' : '+squad_name,  # Adding a title to the chart
+        # Adding a title to the chart
+        title=str(classement+1)+' : '+squad_name,
         polar=dict(
             radialaxis=dict(
                 range=[0, 2.5],
@@ -102,6 +113,5 @@ def get_figure(data : pd.DataFrame, team : str, categories : list[str], classeme
             )
         )
     )
-    
-    
+
     return fig
--- Code/visu2_pretraitement.py
+++ Code/visu2_pretraitement.py
@@ -1,43 +1,50 @@
 import numpy as np
 import pandas as pd
 
-def get_data(data_teams_attack : pd.DataFrame, 
-             data_players : pd.DataFrame, 
-             data_tournament : pd.DataFrame, 
-             data_teams_defense : pd.DataFrame) -> pd.DataFrame :
+
+def get_data(data_teams_attack: pd.DataFrame,
+             data_players: pd.DataFrame,
+             data_tournament: pd.DataFrame,
+             data_teams_defense: pd.DataFrame) -> pd.DataFrame:
 
     data_teams_attack = data_teams_attack.copy()
     data_teams_defense = data_teams_defense.copy()
 
-    data_teams_defense['Squad'] = data_teams_defense['Squad'].str.replace('vs ', '')
-    
+    data_teams_defense['Squad'] = data_teams_defense['Squad'].str.replace(
+        'vs ', '')
+
     # Attack
-    new_df_attack = data_teams_attack[['Squad', 'Poss', 'Gls90', 'Ast90', 'G-PK90']].copy()
-    new_names = {'Poss' : 'Attack_Poss',
-                 'Gls90' : 'Attack_Goals',
-                 'Ast90' : 'Attack_Assists',
-                 'G-PK90' : 'Attack_Non_Penalty_Goals'}
+    new_df_attack = data_teams_attack[[
+        'Squad', 'Poss', 'Gls90', 'Ast90', 'G-PK90']].copy()
+    new_names = {'Poss': 'Passes tentées',
+                 'Gls90': 'Buts marqués',
+                 'Ast90': 'Passes décisives',
+                 'G-PK90': 'Buts sans pénalité'}
     new_df_attack.rename(columns=new_names, inplace=True)
-    new_df_attack['Attack_Poss'] = new_df_attack['Attack_Poss']/100
+    new_df_attack['Passes tentées'] = new_df_attack['Passes tentées']/100
     new_df_attack['Attack_Sum'] = new_df_attack.iloc[:, 1:5].sum(axis=1)
-    
-    
+
     # Defense
-    new_df_defense = data_teams_defense[['Squad', 'Poss', 'Gls90', 'Ast90', 'G-PK90']].copy()
-    new_names = {'Poss' : 'Defense_Poss',
-                 'Gls90' : 'Defense_Goals',
-                 'Ast90' : 'Defense_Assists',
-                 'G-PK90' : 'Defense_Non_Penalty_Goals'}
+    new_df_defense = data_teams_defense[[
+        'Squad', 'Poss', 'Gls90', 'Ast90', 'G-PK90']].copy()
+    new_names = {'Poss': 'Passes adverses tentées',
+                 'Gls90': 'Buts reçus',
+                 'Ast90': 'Passes décisives adverses',
+                 'G-PK90': 'Buts sans pénalités reçus'}
     new_df_defense.rename(columns=new_names, inplace=True)
-    new_df_defense['Defense_Poss'] = (new_df_defense['Defense_Poss'].max() - new_df_defense['Defense_Poss'])/100
-    new_df_defense['Defense_Goals'] = new_df_defense['Defense_Goals'].max() - new_df_defense['Defense_Goals']
-    new_df_defense['Defense_Assists'] = new_df_defense['Defense_Assists'].max() - new_df_defense['Defense_Assists']
-    new_df_defense['Defense_Non_Penalty_Goals'] = new_df_defense['Defense_Non_Penalty_Goals'].max() - new_df_defense['Defense_Non_Penalty_Goals']
+    new_df_defense['Passes adverses tentées'] = 1 - \
+        new_df_attack['Passes tentées']/100
+    new_df_defense['Buts reçus'] = new_df_defense['Buts reçus'].max(
+    ) - new_df_defense['Buts reçus']
+    new_df_defense['Passes décisives adverses'] = new_df_defense['Passes décisives adverses'].max(
+    ) - new_df_defense['Passes décisives adverses']
+    new_df_defense['Buts sans pénalités reçus'] = new_df_defense['Buts sans pénalités reçus'].max(
+    ) - new_df_defense['Buts sans pénalités reçus']
     new_df_defense['Defense_Sum'] = new_df_defense.iloc[:, 1:5].sum(axis=1)
-    
+
     # Merge
     merged_df = pd.merge(new_df_attack, new_df_defense, on='Squad')
-    
-    #print(merged_df)
-    
+
+    print(merged_df)
+
     return merged_df
