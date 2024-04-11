3eb7d91b7cae2fbf1905425c95b3a6546801ec8a
Laure-Anne Réau <laure-anne-margaux.reau@polymtl.ca>
2024-04-11 19:56:51 -0400

    add description for each viz

--- Code/app.py
+++ Code/app.py
@@ -37,6 +37,32 @@ images = {
     'Zambia': 'https://ipdata.co/flags/zm.png',
 }
 
+descriptions = [
+    "Voici l'arbre des matchs de la CAN 2023 à partir des 8e de finale. Votre équipe sélectionnée s'affiche en rouge. \
+        Survolez un pays en particulier pour voir le détail de ses matchs et son avancée dans la compétition. \
+        Le détail pour la 3e place est visible en survolant les équipes de la demi-finale.",
+    "Voici le score moyen des équipes en terme de buts, buts sans pénalty, passes décisives et possession ramenés sur 90 minutes. \
+        Vous pouvez choisir entre les critères d'attaque et de défense. \
+        Les équipes sont classées par ordre décroissant de score. On affiche les 8 meilleures et votre équipe sélectionnée. \
+        Survolez une équipe pour voir son score détaillé.",
+    "Voici la répartition des joueurs de chaque équipe par âge, score moyen et pourcentage de fatigue. \
+        Vous pouvez choisir entre les critères de score, d'âge et de fatigue. \
+        Le score d'un joueur correspond à la somme des buts marqués, buts hors pénalty et passes décisives, ramenée sur 90 minutes. \
+        L'âge d'un joueur correspond à son âge au moment de la coupe d'Afrique des Nations. \
+        La fatigue d'un joueur correspond au temps total passé sur le terrain ramené sur 90 minutes. \
+            Ainsi, un joueur ayant 80% en fatigue aura passé 80% de son temps de jeu sur le terrain. \
+        Les joueurs sont répartis par avancée de leur équipe dans la compétition. \
+        Survolez un point pour voir le détail du joueur.",     
+    "Voici la répartition des équipes par région d'Afrique. \
+        Il y a 5 régions : Nord, Est, Ouest, Centrale et Australe. \
+        Sur la carte, la région en rouge correspond à la région de votre pays sélectionné. \
+        Survolez un pays pour voir à quelle région il appartient. \
+        Sur le graphique en barres, vous pouvez voir le total de buts marqués, passes décisives et possessions par région d'Afrique. \
+        Vous pouvez choisir entre les critères d'attaque et de défense. \
+        Vous pouvez sélectionner et désélectionner des types d'action en cliquant sur les éléments de la légende. \
+        La région encadrée en rouge correspond à la région de votre pays sélectionné."
+    ]
+
 import Code.visu1 as visu1
 import Code.visu2 as visu2
 import Code.visu3 as visu3
@@ -80,7 +106,8 @@ all_data = [data_1, data_2, data_3, data_4]
 
 # Define the layout of the dashboard
 app.layout = html.Div([
-    html.H1("Caractérisation des équipes de la Coupe d'Afrique de Nations (CAN)", style={'textAlign': 'center'}),
+    html.H1("Caractérisation des équipes de la Coupe d'Afrique des Nations (CAN)", style={'textAlign': 'center'}),
+    html.P("Sélectionnez une équipe pour voir ses caractéristiques:", style={'textAlign': 'center'}),
     dcc.Dropdown(
         id='team-selector',
         options=[{'label': html.Div([html.Img(src=images[country], style={'width': '20px', 'height': '10px', 'padding-right' :'5px'}), country]), 'value': country} for country in data_init],
@@ -88,7 +115,7 @@ app.layout = html.Div([
         value="Côte d'Ivoire",
         style={'width': '50%', 'margin': '20px auto', 'textAlign': 'center'}
     ),
-    html.Div(id='visu-container')
+    html.Div(id='visu-container'),
 ])
 
 
@@ -98,8 +125,12 @@ app.layout = html.Div([
     [Input('team-selector', 'value')]
 )
 def update_graphs(selected_value):
-    graphs = [html.Div(visu.get_section(data=all_data[i], team=selected_value), className=f'part_{i+1}') 
-              for i, visu in enumerate([visu1,visu2,visu3,visu4])]
+    graphs = [
+        html.Div([
+            html.P(descriptions[i], style={'textAlign': 'center'}),
+            html.Div(visu.get_section(data=all_data[i], team=selected_value), className=f'part_{i+1}')
+        ]) for i, visu in enumerate([visu1, visu2, visu3, visu4])
+    ]
     return graphs
 
 @app.callback(
